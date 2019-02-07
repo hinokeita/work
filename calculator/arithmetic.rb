ddc503eacbb0f64fe9a4404e090c71a0c98c59ec
hinokeita <hino@sackle.co.jp>
2019-02-07 03:24:01 +0000

    modify:演算子の優先順を変更

--- calculator/arithmetic.rb
+++ calculator/arithmetic.rb
@@ -19,4 +19,19 @@ module Arithmetic
     puts '数値を入力してください'
     x = gets.to_i
   end
+
+  def temp_func(result,temp_num,temp_ope)
+    if (!(temp_num.empty?) && temp_ope == "+")
+      result = add(temp_num[0],temp_num[1])
+    elsif (!(temp_num.empty?) && temp_ope == "-")
+      result = sub(temp_num[0],temp_num[1])
+    end
+  end
+
+  def temp_set(temp_num,result)
+    temp_num.clear
+    temp_num.push(result)
+    temp_num.push(num_scan)
+    return temp_num
+  end
 end
--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -2,37 +2,55 @@ require './arithmetic'
 
 include(Arithmetic)
 inp1 = num_scan
+temp_num = []
+temp_ope = ""
+result = 0
 puts '「＋」「－」「*」「/」のいずれかを入力してください'
 ope1 = gets.chomp
 inp2 = num_scan
 case ope1
 when "+"
-  result = add(inp1,inp2)
+  temp_num.push(inp1)
+  temp_num.push(inp2)
+  temp_ope = "+"
 when "-"
-  result = sub(inp1,inp2)
+  temp_num.push(inp1)
+  temp_num.push(inp2)
+  temp_ope = "-"
 when "×","*"
   result = multi(inp1,inp2)
 when "÷","/"
   result = div(inp1,inp2)
 end
 
-begin
+loop do
   puts '「＋」「－」「*」「/」「=」のいずれかを入力してください'
   ope2 = gets.chomp
   case ope2
   when "+"
-    inp3 = num_scan
-    result = add(result,inp3)
+    result = temp_func(result,temp_num,temp_ope)
+    temp_num = temp_set(temp_num,result)
+    temp_ope = "+"
   when "-"
-    inp3 = num_scan
-    result = sub(result,inp3)
+    result = temp_func(result,temp_num,temp_ope)
+    temp_num = temp_set(temp_num,result)
+    temp_ope = "-"
   when "×","*"
     inp3 = num_scan
-    result = multi(result,inp3)
+    result += multi(inp3,temp_num[1])
+    temp_num[1] = result
+    result = temp_func(result,temp_num,temp_ope)
+    temp_num.clear
   when "÷","/"
     inp3 = num_scan
-    result = div(result,inp3)
+    result += div(inp3,temp_num[1])
+    temp_num[1] = result
+    result = temp_func(result,temp_num,temp_ope)
+    temp_num.clear
+  when "="
+    result = temp_func(result,temp_num,temp_ope) unless temp_num.empty?
+    break
   end
-end while ope2 != "="
+end
 
 puts "計算結果は#{result}です"
